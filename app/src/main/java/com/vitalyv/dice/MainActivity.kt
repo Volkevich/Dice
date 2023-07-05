99235f2f3063084298185bef4d9a343777da2bd9
Vitaly Volkevich <87798074+Volkevich@users.noreply.github.com>
2023-07-05 07:31:36 +0300

    Initial commit

---
+++ app/src/main/java/com/vitalyv/dice/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.vitalyv.dice
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.vitalyv.dice.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.generatedButton.setOnClickListener{
+            onGenerationsClick()
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun onGenerationsClick() {
+        binding.rootLayout?.setBackgroundResource(R.drawable.new_background)
+        val randomNumber = Random.nextInt(1, 7)
+
+        when (randomNumber) {
+            1 -> binding.imageView?.setImageResource(R.drawable.dice1)
+            2 -> binding.imageView?.setImageResource(R.drawable.dice2)
+            3 -> binding.imageView?.setImageResource(R.drawable.dice3)
+            4 -> binding.imageView?.setImageResource(R.drawable.dice4)
+            5 -> {
+                binding.imageView?.let {
+                    Glide.with(this)
+                        .asGif()
+                        .load(R.raw.dicpfive)
+                        .into(it)
+                }
+            }
+            6 -> {
+                binding.imageView?.let {
+                    Glide.with(this)
+                        .asGif()
+                        .load(R.raw.dicpe2)
+                        .into(it)
+                }
+            }
+        }
+    }
+
+
+    }
